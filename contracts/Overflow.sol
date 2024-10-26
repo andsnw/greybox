af2172e882f33e9f25d9edbfc7cbae8a70af15a3
a <github@sl.snow.cx>
2024-10-26 18:35:31 +1100

    update contracts with explainer of vulns

--- contracts/Overflow.sol
+++ contracts/Overflow.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract Overflow {
-    mapping(address => uint256) public accountBalances;
+    mapping(address => uint256) public balances;
     mapping(address => uint256) public withdrawalUnlockTime;
 
     // Allows anyone to deposit ETH and set a lock time
     function depositFunds() external payable {
+        // Vulnerability: Potential integer overflow in unchecked block
         unchecked {
             accountBalances[msg.sender] += msg.value;
         }
         withdrawalUnlockTime[msg.sender] = block.timestamp + 1 weeks;
     }
 
-    // Vulnerable to overflow, the user could pass a very large value causing overflow
+    // Vulnerability: Potential integer overflow, the user could pass a very large value causing overflow
     function extendLockTime(uint256 _additionalSeconds) public {
         unchecked {
             withdrawalUnlockTime[msg.sender] += _additionalSeconds;
@@ -32,12 +33,14 @@ contract Overflow {
         require(successfulWithdrawal, "Failed to send Ether");
     }
 
+    // Vulnerability: Potential integer overflow in unchecked block
     function add(uint256 a, uint256 b) public pure returns (uint256) {
         unchecked {
             return a + b;
         }
     }
 
+    // Vulnerability: Potential integer underflow in unchecked block
     function subtract(uint256 a, uint256 b) public pure returns (uint256) {
         unchecked {
             return a - b;
